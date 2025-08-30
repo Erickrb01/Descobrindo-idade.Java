b40c5a9ba88329654614587c6356538d95eef240
Erick Rodrigues <erickrodriguesbrilhante2003@gmail.com>
2025-08-30 19:56:49 -0300

    Código criando forma.

---
+++ DescobrindoIdade/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner leitorInteiro = new Scanner(System.in);
+
+        int idade = 0;
+        int ano = 0;
+
+        System.out.println("Digite a sua idade:\n ");
+        idade = leitorInteiro.nextInt();
+
+        System.out.println("Digite o ano atual:\n ");
+    }
+}
